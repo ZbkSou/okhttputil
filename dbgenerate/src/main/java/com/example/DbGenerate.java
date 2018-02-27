756c1d3c67ceccedc81cfd76a6733730628347a3
baokangzhou <zhoubk1451@163.com>
2018-02-27 19:15:56 +0800

    多线程下载

--- dbgenerate/src/main/java/com/example/DbGenerate.java
+++ dbgenerate/src/main/java/com/example/DbGenerate.java
@@ -1,14 +1,25 @@
 package com.example;
 
+import org.greenrobot.greendao.generator.DaoGenerator;
 import org.greenrobot.greendao.generator.Entity;
 import org.greenrobot.greendao.generator.Schema;
 
 public class DbGenerate {
     public static void main(String args[]){
-        Schema schema = new Schema(1,"okhttp");
+        Schema schema = new Schema(1,"okhttp.db");
         Entity entity =  schema.addEntity("DownloadEntity");
 
         entity.addLongProperty("start_position");
+        entity.addLongProperty("end_position");
+        entity.addLongProperty("progress_position");
+        entity.addStringProperty("download_url");
+        entity.addIntProperty("thread_id");
+        entity.addIdProperty().autoincrement();
+        try{
+            new DaoGenerator().generateAll(schema,"dbgenerate/src-gen");
 
+        }catch (Exception e){
+
+        }
     }
 }
